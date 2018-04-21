951721d64cfd7d1dd72afb3d1076d3f055988519
Asaf Ben-Hamo <asaf.bh25@gmail.com>
2018-04-21 22:45:10 +0300

    React files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import Lodash from 'lodash';   // you can do : import _ from 'lodash';
+import React ,{ Component } from 'react';
+import ReactDom from 'react-dom';
+import YTSearce from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAYbIR9KaV0e1UzdFS0mv27y0BvewjM3c0';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+ //  (1) Create new component. this component make HTML.
+
+class App extends Component {
+    
+    constructor(props){
+       super(props);
+
+       this.state = { 
+       	              videos: [],
+                      selectedVideo:null
+       	            };
+
+        this.videoSearch('motorcycle');	            
+
+    }
+
+
+    videoSearch(term){
+         YTSearce( { key: API_KEY, term: term } , (videos) => {
+           this.setState( { 
+           	                videos: videos,
+                            selectedVideo: videos[0]
+           	              } );
+         } );
+    }
+
+
+
+	render(){
+       const videoSearch = Lodash.debounce( (term) => { this.videoSearch(term) } , 300 );
+
+
+       return (
+			  <div>
+		         <SearchBar onSearchTermChange={ videoSearch } />
+		         <VideoDetail video={this.state.selectedVideo} />
+		         <VideoList
+		          onVideoSelect={ selectedVideo => this.setState( { selectedVideo} ) }
+		          videos={this.state.videos}
+		         />
+			  </div>
+	       );
+
+	}
+
+
+
+}
+
+
+/// (2) take this component and put it into the page.
+
+ReactDom.render( <App />,document.querySelector('.container') );
